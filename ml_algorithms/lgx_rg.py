2e0c4da7f60601be9048fe4d3a09abc3f6d63139
Benardi <benardinunes@gmail.com>
2019-09-08 18:37:24 -0300

    Add pydoc for lr and utils modules

--- ml_algorithms/lgx_rg.py
+++ ml_algorithms/lgx_rg.py
@@ -7,11 +7,37 @@ p = vectorize(lambda x: 1 if x >= 0.5 else 0)
 
 
 def h(X, theta):
+    """Logistic regression hypothesis.
+
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param theta: Column vector of model's parameters.
+    :type theta: numpy.array
+
+    :raises: ValueError
+
+    :returns: The probability that each entry belong to class 1.
+    :rtype: numpy.array
+    """
     return g(X.dot(theta))
 
 
 def cost_func(X, y, theta):
+    """Computes the cost function J for Logistic Regression.
 
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param y: Column vector of expected values.
+    :type y: numpy.array
+
+    :param theta: Column vector of model's parameters.
+    :type theta: numpy.array
+
+    :returns: Computed cost.
+    :rtype: float
+    """
     m = len(y)
     J = (1 / m) * ((-y.T).dot(log(h(X, theta)))
                    - ((1 - y).T).dot(log(1 - h(X, theta))))
@@ -19,7 +45,23 @@ def cost_func(X, y, theta):
 
 
 def reg_cost_func(X, y, theta, _lambda):
+    """Computes the regularized cost function J for Logistic Regression.
 
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param y: Column vector of expected values.
+    :type y: numpy.array
+
+    :param theta: Column vector of model's parameters.
+    :type theta: numpy.array
+
+    :param _lambda: The regularization hyperparameter.
+    :type _lambda: float
+
+    :returns: Computed cost with regularization.
+    :rtype: float
+    """
     m = len(y)
     J = - (1 / m) * ((y.T).dot(log(h(X, theta)))
                      + (1 - y.T).dot(log(1 - h(X, theta))))
@@ -28,12 +70,43 @@ def reg_cost_func(X, y, theta, _lambda):
 
 
 def grad(X, y, theta):
+    """Computes the gradient for the parameters theta.
+
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param y: Column vector of expected values.
+    :type y: numpy.array
+
+    :param theta: Column vector of model's parameters.
+    :type theta: numpy.array
+
+    :returns: Gradient column vector.
+    :rtype: numpy.array
+    """
     m = len(y)
     grad = (1 / m) * (X.T).dot(h(X, theta) - y)
     return grad
 
 
 def reg_grad(X, y, theta, _lambda):
+    """Computes the regularized gradient for Logistic Regression.
+
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param y: Column vector of expected values.
+    :type y: numpy.array
+
+    :param theta: Column vector of model's parameters.
+    :type theta: numpy.array
+
+    :param _lambda: The regularization hyperparameter.
+    :type _lambda: float
+
+    :returns: Regularized gradient column vector.
+    :rtype: numpy.array
+    """
     m = len(y)
     grad = zeros(theta.shape)
 
@@ -44,8 +117,32 @@ def reg_grad(X, y, theta, _lambda):
 
 
 def predict_prob(X, theta):
+    """ Produces the probability that the entries belong to class 1.
+
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param theta: Column vector of model's parameters.
+    :type theta: numpy.array
+
+    :raises: ValueError
+
+    :returns: The probability that each entry belong to class 1.
+    :rtype: numpy.array
+    """
     return g(X.dot(theta))
 
 
 def predict(X, theta):
+    """ Classifies each entry as class 1 or class 0.
+
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param theta: Column vector of model's parameters.
+    :type theta: numpy.array
+
+    :returns: Column vector with each entry classification.
+    :rtype: numpy.array
+    """
     return p(predict_prob(X, theta))
--- ml_algorithms/utils.py
+++ ml_algorithms/utils.py
@@ -1,8 +1,37 @@
+"""
+.. module:: utils
+    :synopsis: This module provides routines of interest to different models.
+
+.. moduleauthor:: Benardi Nunes <benardinunes@gmail.com>
+"""
+
 from numpy import zeros, copy, std, mean, float64
 
 
 def gradient_descent(X, y, grad, initial_theta, alpha, num_iters):
+    """This function performs parameter optimization via gradient descent.
 
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :param y: Column vector of expected values.
+    :type y: numpy.array
+
+    :param grad: Routine that generates the partial derivatives given theta.
+    :type grad: numpy.array
+
+    :param initial_theta: Initial value for parameters to be optimized.
+    :type initial_theta: numpy.array
+
+    :param alpha: Learning rate or step size of the optimization.
+    :type alpha: float
+
+    :param num_iters: Number of times the optimization will be performed.
+    :type num_iters: int
+
+    :returns: Optimized model parameters.
+    :rtype: numpy.array
+    """
     m = len(y)
     theta = copy(initial_theta)
     for _ in range(num_iters):
@@ -12,6 +41,20 @@ def gradient_descent(X, y, grad, initial_theta, alpha, num_iters):
 
 
 def numerical_grad(J, theta, err):
+    """Numerically calculates the gradient of a given cost function.
+
+    :param J: Function handle that computes cost given theta.
+    :type J: function
+
+    :param theta: Model parameters.
+    :type theta: numpy.array
+
+    :param err: distance between points where J is evaluated.
+    :type err: float
+
+    :returns: Computed numeric gradient.
+    :rtype: numpy.array
+    """
     num_grad = zeros(theta.shape, dtype=float64)
     perturb = zeros(theta.shape, dtype=float64)
 
@@ -26,6 +69,21 @@ def numerical_grad(J, theta, err):
 
 
 def feature_normalize(X):
+    """Performs Z score normalization in a numeric dataset.
+
+    :param X: Features' dataset plus bias column.
+    :type X: numpy.array
+
+    :returns:
+        - X_norm - Normalized features' dataset.
+        - mu - Mean of each feature
+        - sigma - Standard deviation of each feature.
+
+    :rtype:
+        - X_norm (:py:class: numpy.array)
+        - mu (:py:class: numpy.array)
+        - sigma (:py:class: numpy.array)
+    """
     mu = mean(X)
     sigma = std(X)
 
