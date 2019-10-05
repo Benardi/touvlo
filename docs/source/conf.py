0bded937e9461894eab326441f23fba3556de03d
Benardi <benardinunes@gmail.com>
2019-10-04 22:06:18 -0300

    Add issue and PR templates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,9 +35,9 @@ copyright = '2019, Benardi Nunes'
 author = 'Benardi Nunes'
 
 # The short X.Y version
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2.5'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
